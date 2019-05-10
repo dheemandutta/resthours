5fef0f237014d394f380312a6fa20fccf66892f3
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2019-05-10 18:56:37 +0530

    UploadMedicalInventory

---
+++ TM.RestHour.BL/UploadMedicalInventoryBL.cs
@@ -0,0 +1,20 @@
+﻿using TM.RestHour.DAL;
+using TM.Base.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TM.RestHour.BL
+{
+    public class UploadMedicalInventoryBL
+    {
+        public void ImportMedicalInventory(object dataTable, int vesselId)
+        {
+            UploadMedicalInventoryDAL crewImportDAL = new UploadMedicalInventoryDAL();
+            crewImportDAL.ImportMedicalInventory(dataTable, vesselId);
+        }
+    }
+}
---
+++ TM.RestHour/Controllers/UploadMedicalInventoryController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TM.RestHour.BL;
+using TM.RestHour.Models;
+using TM.Base.Entities;
+using System.Globalization;
+using System.IO;
+using System.Configuration;
+using ExcelDataReader;
+
+namespace TM.RestHour.Controllers
+{
+    public class UploadMedicalInventoryController : Controller
+    {
+        // GET: UploadMedicalInventory
+        [TraceFilterAttribute]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [TraceFilterAttribute]
+        public ActionResult Index(HttpPostedFileBase postedFile)
+        {
+            object dtData = new object();
+            UploadMedicalInventoryBL crewImportBL = new UploadMedicalInventoryBL();
+
+            if (postedFile != null)
+            {
+                //string path = Server.MapPath("~/Uploads/");
+                string path = Server.MapPath(ConfigurationManager.AppSettings["UploadMedicalInventoryPath"].ToString());
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+
+
+                string fileName = string.Empty;
+                fileName = Path.GetFileName(postedFile.FileName);
+
+                if (System.IO.File.Exists(path + fileName))
+                {
+                    System.IO.File.Delete(path + fileName);
+                }
+
+                postedFile.SaveAs(path + Path.GetFileName(postedFile.FileName));
+                ViewBag.Message = "File uploaded successfully.";
+
+
+
+
+
+
+
+
+
+
+
+                string filePath = path + fileName;
+
+                // read file 
+                using (var stream = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read))
+                {
+
+                    // Auto-detect format, supports:
+                    //  - Binary Excel files (2.0-2003 format; *.xls)
+                    //  - OpenXml Excel files (2007 format; *.xlsx)
+                    using (var reader = ExcelReaderFactory.CreateReader(stream))
+                    {
+
+
+
+                        // 2. Use the AsDataSet extension method
+                        var dataSet = reader.AsDataSet(new ExcelDataSetConfiguration()
+                        {
+
+                            // Gets or sets a callback to determine which row is the header row. 
+                            // Only called when UseHeaderRow = true.
+                            ConfigureDataTable = _ => new ExcelDataTableConfiguration
+                            {
+                                UseHeaderRow = false,// Use first row is ColumnName here :D
+                                FilterRow = rowHeader => rowHeader.Depth >= 10
+
+                            }
+
+                        });
+
+                        if (dataSet.Tables.Count > 0)
+                        {
+                            dtData = dataSet.Tables[0];
+                            // Do Something
+                        }
+
+
+
+                        // The result of each spreadsheet is in result.Tables
+                    }
+
+                    crewImportBL.ImportMedicalInventory(dtData, int.Parse(Session["VesselID"].ToString()));        ////////////////////////////////////////
+                }
+
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+            return View();
+        }
+    }
+}
