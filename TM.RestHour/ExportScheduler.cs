b03157748f72e45529080f3c3c34b4c05e34de2a
Bingshu Baidya <bingshu.bk@hotmail.com>
2021-03-11 09:02:25 +0530

    Data Export

---
+++ TM.RestHour/ExportScheduler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Quartz;
+using Quartz.Impl;
+
+namespace TM.RestHour
+{
+    public class ExportScheduler
+    {
+        public static  void Start()
+        {
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            
+            IScheduler scheduler =  factory.GetScheduler();
+             scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<Export>().Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithDailyTimeIntervalSchedule
+                  (s =>
+                     //s.WithIntervalInHours(24)
+                    s.WithIntervalInSeconds(30)
+                    .OnEveryDay()
+                    .StartingDailyAt(TimeOfDay.HourAndMinuteOfDay(0, 0))
+                  )
+                .Build();
+
+             scheduler.ScheduleJob(job, trigger);
+        }
+    }
+}
