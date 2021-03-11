6ed9f9c1bb5ecbea368d8185dd25b06f5b013b9c
Prasenjit Paul <prasenjitpaul100@hotmail.com>
2021-03-11 09:07:45 +0530

    ok

---
+++ TM.RestHour/Controllers/PsychometricsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TM.RestHour.BL;
+using TM.RestHour.Models;
+using TM.Base.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.Collections;
+
+namespace TM.RestHour.Controllers
+{
+    public class PsychometricsController : Controller
+    {
+        // GET: Psychometrics
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
