f83a5475dc69f4350138c67d0beb2a7858ba7113
BingshuBaidya <76859289+BingshuBaidya@users.noreply.github.com>
2021-10-08 09:46:47 +0530

    merged export/import

---
+++ TM.RestHour.ExportImport/TM.RestHour.ExportImport/LogEssential.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+
+namespace TM.RestHour.ExportImport
+{
+    public enum LogTarget
+    {
+        File, EventLog
+    }
+    public abstract class LogBase
+    {
+        protected readonly object lockObj = new object();
+        public abstract void Log(string message);
+    }
+
+    public class FileLogger : LogBase
+    {
+        public string filePath = @"c:\MailSyncLog.txt";
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                using (StreamWriter streamWriter = new StreamWriter(filePath))
+                {
+                    streamWriter.WriteLine(message);
+                    streamWriter.Close();
+                }
+            }
+        }
+    }
+
+    public class EventLogger : LogBase
+    {
+        public override void Log(string message)
+        {
+            lock (lockObj)
+            {
+                EventLog m_EventLog = new EventLog("MailSyncEventLog");
+                m_EventLog.Source = "MailSyncLog";
+                m_EventLog.WriteEntry(message);
+            }
+        }
+    }
+
+
+
+    public static class LogHelper
+    {
+        private static LogBase logger = null;
+        public static void Log(LogTarget target, string message)
+        {
+            switch (target)
+            {
+                case LogTarget.File:
+                    logger = new FileLogger();
+                    logger.Log(message);
+                    break;
+                case LogTarget.EventLog:
+                    logger = new EventLogger();
+                    logger.Log(message);
+                    break;
+                default:
+                    return;
+            }
+        }
+    }
+}
---
+++ TM.RestHour.ExportImport/TM.RestHour.ExportImport/MailServiceConfiguration.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TM.RestHour.ExportImport
+{
+    public class MailServiceConfiguration
+    {
+
+        public int Port { get; set; }
+        public string MailServerDomain { get; set; }
+        public string MailId { get; set; }
+        public string MailPassword { get; set; }
+        public string MailServerType { get; set; }
+        public string AttachmentPath { get; set; }
+        public string SubjectLine { get; set; }
+
+
+        /// <summary>
+        /// Loads the configuration file from the provided location
+        /// </summary>
+        /// <param name="configFilePath">Full path of the configuration file along with file name</param>
+        /// <returns></returns>
+        public static MailServiceConfiguration Load(string configFilePath)
+        {
+            MailServiceConfiguration _returnValue = new MailServiceConfiguration();
+            var serializer = new XmlSerializer(typeof(MailServiceConfiguration));
+
+            try
+            {
+                using (var reader = new StreamReader(configFilePath))
+                {
+                    _returnValue = (MailServiceConfiguration)serializer.Deserialize(reader);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(LogTarget.EventLog, ex.Message);
+                return new MailServiceConfiguration();
+            }
+            return _returnValue;
+        }
+
+        /// <summary>
+        /// Saves the configuration file to the provide location
+        /// </summary>
+        /// <param name="configFilePath">Full path of the configuration file along with file name</param>
+        public void Save(string configFilePath)
+        {
+
+            var serializer = new XmlSerializer(typeof(MailServiceConfiguration));
+
+            try
+            {
+                using (var writer = new StreamWriter(configFilePath))
+                {
+                    serializer.Serialize(writer, this);
+                }
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Log(LogTarget.EventLog, ex.Message);
+            }
+        }
+    }
+}
