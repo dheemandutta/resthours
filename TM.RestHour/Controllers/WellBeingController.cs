efe8a2b9fb83e74d28b5c94e81a4653ae68c0374
prasenjitpauldeep <45768968+prasenjitpauldeep@users.noreply.github.com>
2022-02-03 10:42:26 +0530

    WellBeing - Work and RH

--- TM.RestHour/Controllers/WellBeingController.cs
+++ TM.RestHour/Controllers/WellBeingController.cs
@@ -3,6 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TM.RestHour.BL;
+using TM.RestHour.Models;
+using TM.Base.Entities;
+using System.Globalization;
+using System.Web.Script.Serialization;
+using System.Collections;
+using TM.Compliance;
+using System.Text;
+using TM.Base.Common;
+
+using System.Configuration;
+
+using System.IO;
 
 namespace TM.RestHour.Controllers
 {
@@ -19,9 +32,49 @@ namespace TM.RestHour.Controllers
             return View();
         }
 
+        [TraceFilterAttribute]
         public ActionResult WellBeingWorkAndRestHours()
         {
             return View();
         }
+
+        public JsonResult GetNCDetails(string monthyear)
+        {
+            ReportsBL reportsBL = new ReportsBL();
+            ReportsPOCO reportsPC = new ReportsPOCO();
+            Reports reports = new Reports();
+
+            string month = monthyear.Substring(0, monthyear.Length - 4);
+
+            int mon = (int)((Months)Enum.Parse(typeof(Months), month.Trim()));
+            //reportsPC.MonthName = month;
+            int year = int.Parse(Utilities.GetLast(monthyear, 4));
+
+
+            //string month = reports.SelectedMonthYear.Substring(0, reports.SelectedMonthYear.Length - 4);
+
+            //reportsPC.Month = (int)((Months)Enum.Parse(typeof(Months), month.Trim()));
+            //reportsPC.MonthName = month;
+            //reportsPC.Year = int.Parse(Utilities.GetLast(reports.SelectedMonthYear, 4));
+            //reportsPC.CrewID = reports.CrewID;
+            reportsPC.NCDetailsID = reports.NCDetailsID;
+
+            List<ReportsPOCO> reportsList = new List<ReportsPOCO>();
+            reportsList = reportsBL.GetNCDetails(mon, year, int.Parse(Session["VesselID"].ToString()), int.Parse(Session["UserID"].ToString()));
+            //string[] bookedHours = new string[31];
+            //int row = 0;
+            //foreach (ReportsPOCO item in reportsList)
+            //{
+            //    bookedHours[row] = item.Hours;
+            //    row++;
+            //}
+            var data = reportsList;
+
+
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+            //return Json(new { BookedHours = data }, JsonRequestBehavior.AllowGet);
+        }
+
     }
 }
---
+++ TM.RestHour/Scripts/WellBeing.js
@@ -0,0 +1,253 @@
+﻿
+function GetNonComplianceDetails() {
+
+    var posturl = $('#GetNonComplianceDetails').val();
+    //debugger;
+    var htmlstr2 = '';
+    var statustext2 = false;
+
+    $("#schedule").find("tr:gt(1)").remove();
+
+    $.ajax({
+        url: posturl,
+        data: JSON.stringify({ 'monthyear': $('.monthYearPicker').val() }),
+        type: "POST",
+        contentType: "application/json;charset=utf-8",
+        dataType: "json",
+        async: 'false',
+
+        success: function (result) {
+
+            //$('#yr').text(result.Year);
+            //$('#mn').text(result.MonthName);
+            htmlstr2 = result;
+            statustext2 = true;
+            //console.log(result);
+            var crewid = 0;
+            var previouscrewid = 0;
+            var day = 0;
+            var isnewCrew = false;
+
+            var getUrl = window.location;
+            var baseurl = getUrl.protocol + "//" + getUrl.host; //+ "/" + getUrl.pathname.split('/')[1]  ;
+
+            //var baseurl = $('base').attr('href');
+            var imgpathR = baseurl + "/images/R.png";
+            var imgpathG = baseurl + "/images/G.png";
+
+
+            if (result != null) {
+                //loop in result array
+                var positionCounter = 0;
+
+                $.each(result, function (index, item) {
+                    if (item != null) {
+
+                        var table = $('#schedule');
+                        var trLast = $(table).find("tr:last");
+                        var trnew;
+                        var adjustmentfactor = item.AdjustmentFactor;
+                        //console.log('here');
+                        //console.log(item.DateNumber);
+                        var ignoreRow = '';
+                        //ok
+
+                        day = item.DateNumber;
+                        if (crewid == 0) {
+                            crewid = item.CrewID;
+                            previouscrewid = crewid;
+                        }
+
+                        if (crewid != item.CrewID) {
+                            crewid = item.CrewID;
+
+                        }
+
+                        //crewid = item.CrewID;
+
+
+                        var tdnum = 0;
+                        var colcnt = 0;
+                        //add row
+
+                        if (crewid == previouscrewid && index == 0) { //first iteration 
+                            $('#trclone').attr('id', crewid);
+                            var trid = '#' + crewid;
+                            console.log(trid);
+                            $(trid).children('td').each(function (idx, itm) {  //map data in row 1
+
+                                if (idx == 0) //name
+                                {
+                                    //debugger;
+                                    //clear
+                                    $(this).html('');
+                                    $(this).html(result[index].Name);
+                                    console.log(result[index].Name);
+
+                                    //tdnum++;
+                                }
+                                else if (idx == 1) //regimename
+                                {
+                                    //debugger;
+                                    //clear
+                                    $(this).html('');
+                                    $(this).html(result[index].RegimeName);
+                                    console.log(result[index].RegimeName);
+                                    console.log(idx);
+                                    //tdnum++;
+                                }
+                                else if (idx - 1 == parseInt(day)) {
+
+                                    // debugger;
+                                    if (result[index].isNonCompliant == '1') {
+                                        //clear
+                                        $(this).html('');
+                                        $(this).html('&nbsp;<a href="#" id="myid3" onclick="javascript:GetNCDetails(&quot;' + result[index].NCDetailsID + '&quot;);"> <img src="/images/R.png" width="16" height="16"> </a>');  //NC
+                                    }
+                                    else {
+                                        //clear
+                                        $(this).html('');
+                                        $(this).html('&nbsp;<a href="#" id= "' + result[index].NCDetailsID + '"> <img src="/images/G.png" width="16" height="16"> </a>');   //NA
+                                        console.log('Checking Status');
+                                        console.log(idx);
+                                    }
+                                }
+
+                                tdnum++;
+
+                            });
+
+
+
+                        }
+                        else if (crewid == previouscrewid) { // mathcing rows
+                            var trid = $('#schedule tbody>tr:last');
+                            $(trid).children('td').each(function (idx, itm) {  //map data in row 1
+
+                                if (idx === 0) //name
+                                {
+
+                                    if ($(this).html() == result[index].Name) {
+                                        ignoreRow = false;
+                                    }
+                                    else {
+                                        ignoreRow = true;
+                                    }
+
+                                    //console.log('Repeat Name');
+                                    //console.log(result[index].Name);
+                                    //console.log(ignoreRow);
+                                }
+
+                                if (!ignoreRow) {
+                                    if (idx - 1 == parseInt(day)) {
+                                        //console.log('Checking Status');
+                                        //console.log(result[index].isNonCompliant);
+                                        if (result[index].isNonCompliant == '1') {
+                                            //clear
+                                            $(this).html('');
+                                            $(this).html('&nbsp;<a href="#" id="myid3" onclick="javascript:GetNCDetails(&quot;' + result[index].NCDetailsID + '&quot;);"> <img src=' + imgpathR + ' width="16" height="16"> </a>');  //NC
+                                        }
+                                        else {
+                                            //clear
+                                            $(this).html('');
+                                            $(this).html('&nbsp;<a href="#"  id= "' + result[index].NCDetailsID + '"> <img src=' + imgpathG + ' width="16" height="16"> </a>');  //NA
+                                            console.log('Checking Status2');
+                                            console.log(idx);
+                                        }
+                                    }
+                                }
+
+                                tdnum++;
+
+                            });
+
+                        }
+                        else if (crewid !== previouscrewid) {
+                            trnew = $(trLast).clone();
+                            console.log('here in not equal');
+                            previouscrewid = crewid;
+                            $(trnew).attr('id', crewid);
+                            //clear row
+                            ///////////////////////////////////////////////////
+                            var rowcounter = 0;
+                            var columncounter = 0;
+
+                            // = 0;
+                            $(trnew).children('td').each(function () {
+
+                                $(this).html('');
+                            });
+                            //tdnum = 0;
+
+                            $(trnew).children('td').each(function (idx, itm) {  //map data in row n, where n > 1
+                                if (idx == 0) //name
+                                {
+                                    //debugger;
+                                    // $(this).html(result.BookedHours[index].LastName + ' ' + result.BookedHours[index].FirstName);             
+                                    //clear
+                                    $(this).html('');
+                                    $(this).html(result[index].Name);
+                                    console.log('here');
+                                    console.log(result[index].Name);
+                                    //tdnum++;
+                                }
+                                else if (idx == 1) //name
+                                {
+                                    //debugger;
+                                    // $(this).html(result.BookedHours[index].LastName + ' ' + result.BookedHours[index].FirstName);             
+                                    //clear
+                                    $(this).html('');
+                                    $(this).html(result[index].RegimeName);
+                                    //console.log('here');
+                                    //console.log(result[index].Name);
+                                    //tdnum++;
+                                }
+                                else if (idx - 1 == parseInt(day)) {
+                                    //console.log('Checking Status');
+                                    //console.log(result[index].isNonCompliant);
+                                    if (result[index].isNonCompliant == '1') {
+                                        //clear
+                                        $(this).html('');
+                                        $(this).html('&nbsp;<a href="#" id="myid3" onclick="javascript:GetNCDetails(&quot;' + result[index].NCDetailsID + '&quot;);"> <img src=' + imgpathR + ' width="16" height="16"> </a>');  //NC
+                                    }
+                                    else {
+                                        //clear
+                                        $(this).html('');
+
+                                        $(this).html('&nbsp;<a href="#" id= "' + result[index].NCDetailsID + '"> <img src=' + imgpathG + ' width="16" height="16"> </a>');   //NA
+                                    }
+                                }
+
+
+
+
+
+                                //
+                            });
+
+                            tdnum++;
+                            $('#schedule >tbody:last-child').append(trnew);
+                            //$(trLast).after(trnew);
+
+                        }
+                    }
+
+
+                });
+            }
+
+        }
+        ,
+        error: function (errormessage) {
+            console.log(errormessage.responseText);
+        }
+    });
+
+    if (statustext2) {
+        $('#dvprintForMV').val(htmlstr2);
+        var divtoprint = $('#dvprintForMV');
+
+        Popup1ForMV(htmlstr2);
+    }
+}
